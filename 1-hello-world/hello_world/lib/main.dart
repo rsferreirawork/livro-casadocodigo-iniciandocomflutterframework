083e2c627aa3467569cf6760628b43fe15b37cbe
Ramon Ferreira <rsferreirawork@gmail.com>
2021-05-30 11:50:05 -0300

    project hello world

---
+++ 1-hello-world/hello_world/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        'Hello, world!',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontSize: 30.0),
+      ),
+    )
+  );
+}
