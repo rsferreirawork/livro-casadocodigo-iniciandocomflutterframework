932c3b57ae33a5f37c4f3a77dba40909b8babae7
Ramon Ferreira <rsferreirawork@gmail.com>
2021-06-20 19:19:06 -0300

    Commit 50 percent

---
+++ 8-contact-book/flutter_application_contact_book/android/app/src/main/kotlin/com/example/flutter_application_contact_book/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_application_contact_book
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 8-contact-book/flutter_application_contact_book/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_application_contact_book/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
